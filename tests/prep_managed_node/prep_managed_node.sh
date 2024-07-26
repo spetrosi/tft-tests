8e17922ad49fed91bd7c98e50a0a32de2063b6a4
Sergei Petrosian <30409084+spetrosi@users.noreply.github.com>
2024-07-26 15:56:06 +0200

    Add a separate job to prepare managed node (#7)
    * Add functions to prepare managed nodes
    * Add tests/prep_managed_node/prep_managed_node.sh
    * Add LSR_DEBUG to control whether to print playbooks output to terminal
    * Remove req for library 5n general test, not required for local lib
    * Remove cleanup

---
+++ tests/prep_managed_node/prep_managed_node.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+# Test parameters:
+# REPO_NAME
+#   Name of the role repository to test.
+#
+# GITHUB_ORG
+#   Optional: GitHub org to fetch test repository from. Default: linux-system-roles. Can be set to a fork for test purposes.
+GITHUB_ORG="${GITHUB_ORG:-linux-system-roles}"
+# REQUIRED_VARS
+#   Env variables required by this test
+REQUIRED_VARS=("REPO_NAME")
+# PYTHON_VERSION
+#   Python version to install ansible-core with (EL 8, 9, 10 only).
+
+tmt_tree_provision=${TMT_TREE%/*}/provision
+role_path=$TMT_TREE/$REPO_NAME
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "rlImport library"
+        for required_var in "${REQUIRED_VARS[@]}"; do
+            if [ -z "${!required_var}" ]; then
+                rlDie "This required variable is unset: $required_var "
+            fi
+        done
+        rolesCS8InstallPython
+        rolesDistributeSSHKeys "$tmt_tree_provision"
+        rolesEnableHA
+        rolesDisableNFV
+        rolesGenerateTestDiscs "$role_path"
+    rlPhaseEnd
+rlJournalEnd

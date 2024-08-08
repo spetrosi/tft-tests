0275733f2ace783c2bb666ba4d5b79cfd7c5732c
Sergei Petrosian <30409084+spetrosi@users.noreply.github.com>
2024-08-08 19:16:30 +0200

    Move hardcoding PYTHON_VERSION and ANSIBLE_VER to the same place (#26)
    * Move hardcoding PYTHON_VERSION and ANSIBLE_VER to the same place
    
    Running rlIsFedora outside of phases causes "Assertion not in phase"
    
    * Fix collection_path set twice
    
    * Fix typo

--- library/lib.sh
+++ library/lib.sh
@@ -10,19 +10,22 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 rolesInstallAnsible() {
-    if rlIsRHELLike 7; then
-        # Hardcode to the only supported version on EL 7
-        ANSIBLE_VER=2.9
-    fi
-    if rlIsRHELLike 8 && [ "$ANSIBLE_VER" != "2.9" ]; then
+    # Hardcode to the only supported version on later ELs
+    if rlIsRHELLike 8 && [ "$ANSIBLE_VER" == "2.9" ]; then
+        PYTHON_VERSION=3.9
+    elif rlIsRHELLike 8 && [ "$ANSIBLE_VER" != "2.9" ]; then
         # CentOS-8 supports either 2.9 or 2.16
         ANSIBLE_VER=2.16
+    elif rlIsRHELLike 7; then
+        PYTHON_VERSION=3
+        ANSIBLE_VER=2.9
     fi
+
     if rlIsFedora || (rlIsRHELLike ">7" && [ "$ANSIBLE_VER" != "2.9" ]); then
         rlRun "dnf install python$PYTHON_VERSION-pip -y"
         rlRun "python$PYTHON_VERSION -m pip install ansible-core==$ANSIBLE_VER.* passlib"
     elif rlIsRHELLike 8; then
-        PYTHON_VERSION=3.9
+        # el8 ansible-2.9
         rlRun "dnf install python$PYTHON_VERSION -y"
         # selinux needed for delegate_to: localhost for file, copy, etc.
         # Providing passlib for password_hash module, see https://issues.redhat.com/browse/SYSROLES-81
@@ -137,18 +140,19 @@ rolesIsAnsibleCmdOptionSupported() {
 }
 
 rolesInstallDependencies() {
+    local role_path=$1
+    local collection_path=$2
     local coll_req_file="$1/meta/collection-requirements.yml"
     local coll_test_req_file="$1/tests/collection-requirements.yml"
-    collection_path=$(mktemp --directory -t collections-XXX)
     for req_file in $coll_req_file $coll_test_req_file; do
         if [ ! -f "$req_file" ]; then
             rlLogInfo "Skipping installing dependencies from $req_file, this file doesn't exist"
         else
-            rlRun "ansible-galaxy collection install -p $2 -vv -r $req_file"
-            if rolesIsAnsibleEnvVarSupported ANSIBLE_CONNECTION_PATH; then
-                rlRun "export ANSIBLE_COLLECTIONS_PATH=$2"
+            rlRun "ansible-galaxy collection install -p $collection_path -vv -r $req_file"
+            if rolesIsAnsibleEnvVarSupported ANSIBLE_COLLECTION_PATH; then
+                rlRun "export ANSIBLE_COLLECTIONS_PATH=$collection_path"
             else
-                rlRun "export ANSIBLE_COLLECTIONS_PATHS=$2"
+                rlRun "export ANSIBLE_COLLECTIONS_PATHS=$collection_path"
             fi
             rlLogInfo "$req_file Dependencies were successfully installed"
         fi
